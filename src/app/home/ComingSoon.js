995d363fa4c793c79f602763321072debc96a40b
sak03 <sartaj2394@gmail.com>
2025-06-30 11:06:43 +0530

    files removed

--- src/app/home/ComingSoon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ComingSoon = () => {
-  return (
-    <div className='my-3'>Coming Soon Movies not found</div>
-  )
-}
-
-export default ComingSoon
--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-
-//  export const BASE_URL = "https://movietime-main.wemonde.co/api/"; //PREPROD
-export const BASE_URL = "https://cinema-api.wemonde.co/api/" // STAGE
-export const chain = "MT";
-export const appPlatform = "WEBSITE";
-export const appVersion = "1.0";
-export const countryName = "INDIA";
